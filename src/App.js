560c96d30fbe70b1024934fac0f0b7536d0b2773
Fernando Muzaber <fmuzaber@economicas.uba.ar>
2023-06-03 14:30:53 -0300

    Pink Floyd App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Error404 from "./pages/Error404";
 import Header from "./pages/Header";
 import Inicio from "./pages/Inicio";
 import Footer from "./pages/footer";
 import Integrantes from "./pages/Integrantes";
 import Discografia from "./pages/Discografia";
-import Error404 from "./pages/Error404";
+
 
 function App() {
   return (
@@ -16,7 +17,8 @@ function App() {
         <Route path="/" element={<Inicio/>}></Route>
         <Route path="/Integrantes" element={<Integrantes/>}></Route>
         <Route path="/Discografia" element={<Discografia/>}></Route>
-        <Route path="/*" element={<Error404/>}></Route>
+        <Route path="/*" element={<Inicio/>}></Route>
+        <Route path="*" element={<Error404/>}></Route>
       </Routes>
       <Footer />
       </BrowserRouter>
