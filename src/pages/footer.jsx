09050b35c7906b6c11548262b25b51da02a4b652
Fernando Muzaber <fmuzaber@economicas.uba.ar>
2023-10-14 17:02:26 -0300

    updates gh-pages

---
+++ src/pages/footer.jsx
@@ -0,0 +1,12 @@
+const footer = () => {
+    return (
+        <div className="container bg-dark">
+            <div className="row">
+                <div className="col text-center">
+                    <p> Mi primer desarrollo en React JS </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default footer;
